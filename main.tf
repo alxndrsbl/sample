53e5e4d4bfdd2056238d9b29b5f7df1b986bd8b2
Cloud User <ec2-user@ip-172-31-37-105.us-east-2.compute.internal>
2021-09-06 10:10:37 +0000

    changed pair to name

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "example" {
     ami = "ami-0ba62214afa52bec7"
     instance_type = "t2.micro"
     availability_zone = "us-east-2a"
-    key_pair = "sample-rhel" 
+    key_name = "sample-rhel" 
  
   lifecycle {
     ignore_changes = [ami]
