80138f4e64505b10569f1762f746f6c03d34f734
Cloud User <ec2-user@ip-172-31-37-105.us-east-2.compute.internal>
2021-09-06 07:46:33 +0000

    added quotation marks in ami ID

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 #AWS Instance
 resource "aws_instance" "example" {
-    ami = ami-0ba62214afa52bec7
+    ami = "ami-0ba62214afa52bec7"
     instance_type = "t2.micro"
     availability_zone = "us-east-2a"
   
