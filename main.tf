ac50ee8888a1aeeb65cd7c7f9589ae805029fe64
Cloud User <ec2-user@ip-172-31-37-105.us-east-2.compute.internal>
2021-09-06 07:23:26 +0000

    changed call out values to actual

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+
 #AWS Instance
 resource "aws_instance" "example" {
-    ami = data.aws_ami.windows.id
+    ami = ami-0ba62214afa52bec7
     instance_type = "t2.micro"
-    availability_zone = var.availability_zone
+    availability_zone = "us-east-2a"
   
   lifecycle {
     ignore_changes = [ami]
@@ -29,7 +32,7 @@ data "aws_ami" "rhel" {
 #EBS Volume and Attachment
 
 resource "aws_ebs_volume" "example" {
-  availability_zone = var.availability_zone
+  availability_zone = "us-east-2a"
   size              = 10
 }
 
